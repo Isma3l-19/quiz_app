eb53be76e0f0c3496bf98e4913949642dd873a87
Isma3l-19 <ismaelnyambu.99@gmail.com>
2025-02-26 14:00:52 +0300

    Updated the config file moved the sensitive data to .env file

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os 
+from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'helloworld'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True #it was false
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    LANGUAGES = ['en', 'es', 'sw']
--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 from app import create_app, db
 from flask_migrate import Migrate
+from flask import request
+from flask_babel import Babel
+from config import Config
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = create_app()
+
+babel = Babel(app, locale_selector=get_locale)
 
 migrate = Migrate(app, db)
 
